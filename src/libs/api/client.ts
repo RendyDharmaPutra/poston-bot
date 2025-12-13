253cc310887e6994a6b31ccf8c689b3323c54515
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-12-13 17:12:53 +0700

    feat: axios instance for accessing api

---
+++ src/libs/api/client.ts
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { env } from "../../config/env";
+
+/**
+ * Axios client for making HTTP requests to the API.
+ *
+ * This client is configured with a base URL, timeout, and default headers.
+ * It also includes request and response interceptors for handling errors.
+ */
+export const apiClient = axios.create({
+  baseURL: env.API_BASE_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+/**
+ * Request interceptor.
+ *
+ * This interceptor is called before sending a request and can be used to modify the request configuration.
+ * In this case, it simply returns the config as is.
+ *
+ * @param {AxiosRequestConfig} config - The request configuration.
+ * @returns {AxiosRequestConfig} The modified request configuration.
+ */
+apiClient.interceptors.request.use(
+  (config) => config,
+  (error) => {
+    throw error;
+  }
+);
+
+/**
+ * Response interceptor.
+ *
+ * This interceptor is called after receiving a response and can be used to modify the response.
+ * In this case, it simply returns the response as is.
+ *
+ * @param {AxiosResponse} response - The response object.
+ * @returns {AxiosResponse} The modified response object.
+ */
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    throw error;
+  }
+);
---
+++ src/libs/api/endpoints.ts
@@ -0,0 +1,12 @@
+import { apiClient } from "./client";
+
+/**
+ * Object that contains all the API endpoints.
+ *
+ * Each property of this object represents an API endpoint and its value is the relative URL path.
+ *
+ * @example
+ * // Get the relative URL path for the "posts" endpoint
+ * const postsEndpoint = endpoints.posts; // "/api/posts"
+ */
+export const endpoints = {};
