c00adae05e07bd102bc5ffb59d3ad028d08ce052
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-12-16 22:24:32 +0700

    feat: add rate limiter

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,9 +1,21 @@
-import { Bot } from "grammy";
+import { Bot, Context } from "grammy";
 import { env } from "../config/env";
 import { registerCommands } from "./register/commands";
 import { registerGlobalErrorHandler } from "./register/error";
+import { limit } from "@grammyjs/ratelimiter";
 
 const bot = new Bot(env.BOT_TOKEN);
+
+bot.use(
+  limit({
+    timeFrame: 5000, // 5 detik
+    limit: 3,
+    keyGenerator: (ctx: Context) => ctx.from?.id.toString(),
+    onLimitExceeded: async (ctx: Context) => {
+      await ctx.reply("⚠️ Terlalu banyak permintaan, tunggu 5 detik.");
+    },
+  })
+);
 
 registerCommands(bot);
 registerGlobalErrorHandler(bot);
