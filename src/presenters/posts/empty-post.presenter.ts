71832831b9e0a50578092452e0b46abe8c481870
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-12-16 14:48:08 +0700

    feat: showing empty post message while no post saved

---
+++ src/presenters/posts/empty-post.presenter.ts
@@ -0,0 +1,7 @@
+export function emptyPostPresenter(): string {
+  return (
+    `📭 <b>Belum ada postingan tersimpan</b>\n\n` +
+    `Silakan tambahkan postingan yang ingin disimpan ` +
+    `dengan mengirim <b>tautan postingan</b> ke bot ini.`
+  );
+}
--- src/presenters/posts/posts.presenter.ts
+++ src/presenters/posts/posts.presenter.ts
@@ -1,11 +1,14 @@
 import { PostType } from "../../types/post.type";
 import { MetaApiResponse } from "../../types/api/api-response.type";
 import { postItemPresenter } from "./post-item.presenter";
+import { emptyPostPresenter } from "./empty-post.presenter";
 
 export function listPostPresenter(
   posts: PostType[],
   meta: MetaApiResponse
 ): string {
+  if (posts.length === 0) return emptyPostPresenter();
+
   let text = `📚 <b>Post Tersimpan</b> (${meta.page}/${meta.lastPage})\n`;
   text += `────────────────────\n\n`;
 
